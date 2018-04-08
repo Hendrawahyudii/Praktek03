b9cc5cbeb97c5a59d69a5dd2a168add1a4de1207
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-04-08 15:35:41 +0700

    Getter and Setter

--- src/praktek03/SepatuSendal.java
+++ src/praktek03/SepatuSendal.java
@@ -6,5 +6,15 @@ public class SepatuSendal extends Sendal {
     void throttle(){
         System.out.println("- - - -Arigatuoo- - - -");
     }
+
+    public String getBahan() {
+        return bahan;
+    }
+
+    public void setBahan(String bahan) {
+        this.bahan = bahan;
+    }
+    
+    
     
 }
