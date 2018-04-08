cf1dd9f3c9043bab69d7bb0efb19f2be62cfc535
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-04-08 15:04:28 +0700

    Menggunakan Method set

--- src/praktek03/SendalAksi.java
+++ src/praktek03/SendalAksi.java
@@ -3,9 +3,10 @@ package praktek03;
 public class SendalAksi {
     public static void main(String[] args) {
         Sendal s = new Sendal();
-        s.merek="Eiger";
-        s.warna="Hitam";
-        s.harga=165000;
+
+        s.setMerek("Crocodile");
+        s.setWarna("Hitam");
+        s.setHarga(165000);
         
         s.cetakInfo();
     }
