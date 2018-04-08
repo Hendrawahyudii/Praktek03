044fb06220d4ff7f9ca9b2e1b02155f1accac515
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-04-08 15:33:35 +0700

    Atribute dan method

--- src/praktek03/SepatuSendal.java
+++ src/praktek03/SepatuSendal.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class SepatuSendal extends Sendal {
+    private String bahan;
+    
+    void throttle(){
+        System.out.println("- - - -Arigatuoo- - - -");
+    }
     
 }
