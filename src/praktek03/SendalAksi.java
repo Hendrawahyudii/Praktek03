583314a6f00ba8929bbcf3cb288f5b981eb98b72
Hendrawahyudii <32281589+Hendrawahyudii@users.noreply.github.com>
2018-04-08 14:53:27 +0700

    Instance, buat atribut dan panggil method

--- src/praktek03/SendalAksi.java
+++ src/praktek03/SendalAksi.java
@@ -2,6 +2,11 @@ package praktek03;
 
 public class SendalAksi {
     public static void main(String[] args) {
+        Sendal s = new Sendal();
+        s.merek="Eiger";
+        s.warna="Hitam";
+        s.harga=165000;
         
+        s.cetakInfo();
     }
 }
